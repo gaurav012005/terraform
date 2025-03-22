196227ae84921d2c6c9715adefb3b21dcf827bc4
gaurav012005 <gauravmahadik001@gmail.com>
2025-03-22 17:04:16 +0530

    Update ec2.tf

--- learning/day2/ec2-creation/ec2.tf
+++ learning/day2/ec2-creation/ec2.tf
@@ -1,31 +1,18 @@
-data "aws_ami" "os_image" {
-  owners = ["099720109477"]
-  most_recent = true
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/*amd64*"]
-  }
+resource aws_key_pair my-key {
+    key_name = "terra-key-ec2"
+    public_key = file("terra-key-ec2.pub")
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "terra-automate-key"
-  public_key = file("terra-key.pub")
-}
-
-resource "aws_default_vpc" "default" {
+resource aws_default_vpc default {
 
 }
 
-resource "aws_security_group" "allow_user_to_connect" {
-  name        = "allow TLS"
-  description = "Allow user to connect"
-  vpc_id      = aws_default_vpc.default.id
+resource aws_security_group my_security_group {
+  name        = "automate-sg"
+  description = "This will add tf generated security group"
+  vpc_id = aws_default_vpc.default.id
+
   ingress {
-    description = "port 22 allow"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -33,61 +20,30 @@ resource "aws_security_group" "allow_user_to_connect" {
   }
 
   egress {
-    description = " allow all outgoing traffic "
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "port 80 allow"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "port 443 allow"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
-    Name = "mysecurity"
+    Name = "automate_sg"
   }
+  
 }
 
-resource "aws_instance" "testinstance" {
-  ami             = data.aws_ami.os_image.id
-  instance_type   = var.my_enviroment == "prd" ? "t2.medium" : "t2.micro"  
-  key_name        = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.allow_user_to_connect.name]
-  user_data = file("${path.module}/script.sh")
-  tags = {
-    Name = "Terra-Automate"
-  }
-  root_block_device {
-    volume_size = 10 
-    volume_type = "gp3"
-  }
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("terra-key")
-    host        = self.public_ip
-  }
+resource "aws_instance" "my_instance" {
+    key_name = aws_key_pair.my-key.key_name
+    security_groups = [aws_security_group.my_security_group.name]  
+    instance_type = var.ec2_instance_type
+    ami = var.ec2_ami_id
+    user_data = file("install_nginx.sh")
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y apache2",
-      "sudo systemctl start apache2",
-      "sudo systemctl enable apache2",
-      "echo 'Hello from Terraform Provisioners!' | sudo tee /var/www/html/index.html"
-    ]
-  }
+    root_block_device {
+        volume_size = var.ec2_root_storage_size
+        volume_type = "gp3"
+    }
+    tags = {
+      Name = "ec2-instance"
+    } 
 }
